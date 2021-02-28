e729dbf65902cb5a31a41893616c1595a69e2856
harsh-kumar-saaniya <harshvithal1@gmail.com>
2021-02-28 01:44:36 -0800

    Sucessfully Intialize react native app

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import SplashScreen from './src/Screens/SplashScreen';
+const App = () => {
+  return (
+    <View>
+      <SplashScreen />
+      <Text> har har mahadev</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ src/Screens/SplashScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SplashScreen = () => {
+    return (
+        <View>
+            <Text>Slash screen</Text>
+        </View>
+    )
+}
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({})
